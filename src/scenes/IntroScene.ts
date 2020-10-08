c344e2add2634eeedbb99465cd3b726551cc21ae
vinhub <vin.bhalerao@gmail.com>
2020-10-07 21:55:50 -0700

    typo fix

--- src/scenes/IntroScene.ts
+++ src/scenes/IntroScene.ts
@@ -30,7 +30,7 @@ export default class IntroScene extends Phaser.Scene
             'Wealth inequality has been in the news a lot lately. Here is an interactive guide to help you see how wealth inequality emerges in a society and what factors contribute to it.',
             '',
             '(The purpose of this guide is purely to educate people about this important topic so they can make informed decisions about how to deal with it. ' +
-            'Also note that this is essentially just the first chapter in this guide. We hope to add more and more nucances to it over time.)',
+            'Also note that this is essentially just the first chapter in this guide. We hope to add more and more nuances to it over time.)',
             '',
             'Let us start with a very simple game:',
             '',
